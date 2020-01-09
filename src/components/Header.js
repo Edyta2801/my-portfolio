0d9dc04487eea81d1bad9ce0b9419830e5c24400
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-09 21:03:48 +0100

    modify Header component and add style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,37 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import styles from '../components/styles/Header.scss';
+// import Icon from '../../common/Icon/Icon';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
-export default () => <header>MENU!</header>
+class Header extends React.Component {
+    render() {
+        return (
+            <header className={styles.component}>
+                <Grid>
+                    <Row between="md" middle="xs">
+                        <Col md={1} lg={2}>
+                            <Link to='/'>
+                                <div className={styles.logo}>
+                                    {/* <Icon name='laptop-code' /> */}
+                                    <div className={styles.name}>Edyta Szarowska</div>
+                                    <div className={styles.nameJunior}> Junior Front-End Developer</div>
+                                </div>
+                            </Link>
+                        </Col>
+                        <Col md={6}>
+                            <nav>
+                                <NavLink to='/projects' activeClassName='active'>Projects</NavLink>
+                                <NavLink to='/about' activeClassName='active'>About</NavLink>
+                                <NavLink to='/contact' activeClassName='active'>Contact</NavLink>
+                            </nav>
+                        </Col>
+
+                    </Row>
+                </Grid>
+            </header>
+        );
+    }
+}
+
+export default Header;
