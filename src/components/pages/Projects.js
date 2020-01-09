bcee33287e177c416a2e9d8378b382e14fc49bae
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-10 00:16:01 +0100

    add bootstrap cards ver. 1

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,82 @@
 import React from 'react';
+import { Card, CardDeck } from 'react-bootstrap';
 
-export default () => <h1>Projects</h1>
+
+
+class Projects extends React.Component {
+    render() {
+        return (
+            <div>
+                <CardDeck>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                        <Card.Img variant="top" src="https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                        <small className="text-muted">Last updated 3 mins ago</small>
+
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src=" https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+
+                        <small className="text-muted">Last updated 3 mins ago</small>
+
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                        <Card.Img variant="top" src=" https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                        <small className="text-muted">Last updated 3 mins ago</small>
+
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src=" https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+      </Card.Text>
+                        </Card.Body>
+
+                        <small className="text-muted">Last updated 3 mins ago</small>
+
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+      </Card.Text>
+                        </Card.Body>
+                        <Card.Img variant="top" src=" https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                        <small className="text-muted">Last updated 3 mins ago</small>
+
+                    </Card>
+                </CardDeck>
+
+
+            </div>
+        );
+    }
+}
+
+export default Projects;
