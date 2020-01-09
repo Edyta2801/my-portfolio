d0e8574ccbcc4079dea23bd77c8119c5a6f477ec
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-09 15:54:06 +0100

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Projects} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </main>
+    )
+  }
 }
 
 export default App;
