e34aa6d0f70551ebfc451bdab88850789c73dc6d
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-10 22:05:13 +0100

    add hamburger menu with bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,42 @@ import { NavLink, Link } from 'react-router-dom';
 import '../components/styles/Header.scss';
 // import Icon from '../../common/Icon/Icon';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import '../components/styles/App-style.css';
+import '../components/styles/Responsive.css';
+import '../components/styles/Hamburgers.css';
 
 class Header extends React.Component {
+    state = {
+        hamburgerState: "off"
+    }
+
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+
+    toggleMenu = () => {
+        let hamburger = document.querySelector(".hamburger");
+        let overlay = document.querySelector(".overlay");
+        if (this.state.hamburgerState === "off") {
+            hamburger.classList.add("is-active");
+            overlay.classList.remove("hide");
+            overlay.classList.add("show");
+            this.setState({ hamburgerState: "on" })
+        } else {
+            hamburger.classList.remove("is-active");
+            overlay.classList.remove("show");
+            overlay.classList.add("hide");
+            this.setState({ hamburgerState: "off" })
+        }
+    }
     render() {
         return (
             <header className='component'>
+                <button className="hamburger hamburger--spin mobile" type="button" onClick={this.toggleMenu}>
+                    <span className="hamburger-box">
+                        <span className="hamburger-inner"></span>
+                    </span>
+                </button>
                 <Grid>
                     <Row between="md" middle="xs">
                         <Col>
@@ -22,6 +53,13 @@ class Header extends React.Component {
                         </Col>
                         <Col md={6}>
                             <nav>
+                                <div className="overlay mobile">
+                                    <div className="submenus">
+                                        <div className="work-mobile"><Link to="/" className="active">Work</Link></div>
+                                        <div className="articles-mobile"><Link to="/articles">Articles</Link></div>
+                                        <div className="about-mobile"><Link to="/about">About</Link></div>
+                                    </div>
+                                </div>
                                 <NavLink to='/projects' activeClassName='active'>Projects</NavLink>
                                 <NavLink to='/about' activeClassName='active'>About</NavLink>
                                 <NavLink to='/contact' activeClassName='active'>Contact</NavLink>
@@ -29,7 +67,7 @@ class Header extends React.Component {
                         </Col>
                     </Row>
                 </Grid>
-            </header>
+            </header >
         );
     }
 }
