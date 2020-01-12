7ccc72dce8fa7ea9d109db285d5e676026ba54b5
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-12 20:40:01 +0100

    modify index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/index.css';
+
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  < BrowserRouter>
     <App />
   </BrowserRouter>, document.getElementById('root'));
