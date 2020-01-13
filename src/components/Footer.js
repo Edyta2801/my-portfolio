6362b0b9a358cd466a832b615cdf7e46b808bf52
Edyta2801 <edyta.szarowska@interia.pl>
2020-01-14 00:16:42 +0100

    modify Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../components/styles/App-style.css';
+import '../components/styles/Responsive.css';
 
-export default () => <h1>Footer</h1>
+import github from '../images/GitHub-logo.png';
+import linkedin from '../images/linkedin-logo.png';
+import email from '../images/email-logo.png';
+
+class Footer extends Component {
+  state={
+    linkedin: linkedin,
+    email: email,
+    github:github,
+  }
+
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-statement">
+          Let's work together!
+        </div>
+        <div className="social-links">
+          <div className="social">
+            <a href="https://www.linkedin.com/in/edyta-szarowska/" target="_blank" rel="noopener noreferrer">
+              <img className="linkedin" src={this.state.linkedin} alt="Linkedin Logo"/>
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://github.com/Edyta2801" target="_blank" rel="noopener noreferrer">
+              <img className="github" src={this.state.github} alt="Medium Logo"/>
+            </a>
+          </div>
+          <div className="social-last">
+            <a href="mailto:edyta.szarowska@interia.pl" target="_blank" rel="noopener noreferrer">
+              <img className="email" src={this.state.email} alt="Email Logo"/>
+            </a>
+          </div>
+        </div>
+        <div className="footer-copyright">
+          © 2020, Edyta Szarowska
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
