425d0693149e91c49399d79b7180ca2e267372c8
Edyta2801 <edyta.szarowska@interia.pl>
2020-02-24 23:12:10 +0100

    delete files

--- src/images/images.js
+++
@@ -1,7 +0,0 @@
-const images = [
-    { id: 1, src: '.', title: 'foo', description: 'bar' },
-    { id: 2, src: './assets/image02.jpg', title: 'foo', description: 'bar' },
-
-];
-
-export default images;
