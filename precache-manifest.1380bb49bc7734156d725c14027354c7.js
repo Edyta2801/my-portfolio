eb4ccf5f4cef8703cc5baff6bb678fc359e5a6c5
Edyta2801 <edyta.szarowska@interia.pl>
2020-02-25 12:15:04 +0100

    Updates

---
+++ precache-manifest.1380bb49bc7734156d725c14027354c7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "924568c88e7d5d7629b40d4e9ff83e15",
+    "url": "/portfolio-3/static/media/memory_new1.924568c8.png"
+  },
+  {
+    "revision": "c42e6394ced3df3249a2",
+    "url": "/portfolio-3/static/css/main.a41c9c02.chunk.css"
+  },
+  {
+    "revision": "146ec39749a1ef49442c",
+    "url": "/portfolio-3/static/js/runtime~main.3dbb8ade.js"
+  },
+  {
+    "revision": "744825db86343ca266f2b4cb5b4fe477",
+    "url": "/portfolio-3/static/media/Edyta_Szarowska_CV_.744825db.pdf"
+  },
+  {
+    "revision": "eefe9c4572916919e8f2",
+    "url": "/portfolio-3/static/js/2.9a0df8a4.chunk.js"
+  },
+  {
+    "revision": "f79617896e543ca81b4d25fc0d91ff7a",
+    "url": "/portfolio-3/static/media/blog_1.f7961789.png"
+  },
+  {
+    "revision": "2434ccd1e9ee293c1d8945e87ddbdc73",
+    "url": "/portfolio-3/static/media/portfolio.2434ccd1.jpg"
+  },
+  {
+    "revision": "df959316d15a9bb38027c4edf44cbc98",
+    "url": "/portfolio-3/static/media/test.df959316.png"
+  },
+  {
+    "revision": "c42e6394ced3df3249a2",
+    "url": "/portfolio-3/static/js/main.d42b9305.chunk.js"
+  },
+  {
+    "revision": "d7b14b410aa31f2535c349a4b8a32b25",
+    "url": "/portfolio-3/static/media/webpage_1_.d7b14b41.png"
+  },
+  {
+    "revision": "0ab7370b521888e0b00ebf7fa4c3f82e",
+    "url": "/portfolio-3/static/media/pizzeria_new.0ab7370b.png"
+  },
+  {
+    "revision": "11a867bc34f1b98900b3bdae69a22344",
+    "url": "/portfolio-3/static/media/travel.11a867bc.png"
+  },
+  {
+    "revision": "1fc832b0b39c0b607583ab8f8eff7848",
+    "url": "/portfolio-3/static/media/to_do.1fc832b0.png"
+  },
+  {
+    "revision": "1213374d4ff8bd8f4a664659a5bd151e",
+    "url": "/portfolio-3/static/media/admin_panel.1213374d.jpg"
+  },
+  {
+    "revision": "eefe9c4572916919e8f2",
+    "url": "/portfolio-3/static/css/2.76ddcf51.chunk.css"
+  },
+  {
+    "revision": "17096cc7c1dd7d869c1953ddf4f1fadf",
+    "url": "/portfolio-3/index.html"
+  }
+];
