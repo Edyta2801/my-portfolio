0970737d0e3552d8b3e36b0e7026bcbcb44d8a98
Edyta2801 <edyta.szarowska@interia.pl>
2021-04-26 22:51:49 +0200

    add new icon with bugs

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,144 +1,165 @@
-import React, { Component } from 'react';
-import '../styles/App-style.css';
-import '../styles/Responsive.css';
-// import "../styles/Skills.scss";
+import React, { Component } from "react";
+import "../styles/App-style.css";
+import "../styles/Responsive.css";
+import "../styles/About.css";
 //bootstrap
 import { Row, Col, Card, CardDeck } from "react-bootstrap";
 //fontawesome
+// import Image from 'react-bootstrap/Image';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLayerGroup, faUsers } from "@fortawesome/free-solid-svg-icons";
 import {
-    faLayerGroup,
-    faUsers
-} from "@fortawesome/free-solid-svg-icons";
-import {
-    faHtml5,
-    faCss3Alt,
-    faSass,
-    faBootstrap,
-    faJs,
-    faReact,
-    faNode,
-    faEnvira,
-    faGitAlt,
-    faNpm,
-    faYarn,
-    faJira,
-    faSlack,
-
+  faHtml5,
+  faCss3Alt,
+  faSass,
+  faBootstrap,
+  faJs,
+  faReact,
+  faNode,
+  faEnvira,
+  faGitAlt,
+  faNpm,
+  faYarn,
+  faJira,
+  faSlack,
 } from "@fortawesome/free-brands-svg-icons";
 
+import Chakra from "../../images/skills/logo-black.svg";
+import Cypress from "../../images/skills/cypress-logo.webp";
+import Jwt from "../../images/skills/logo-jwt.svg";
+import Msw from "../../images/skills/msw-logo.svg";
+import Storybook from "../../images/skills/storybook.svg";
+import Swagger from "../../images/skills/sw_full-wt.svg";
 
 class About extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-
-    componentDidMount() {
-        window.scrollTo(0, 0);
-    }
-
-
-    render() {
-        return (
-            <div className="body skills">
-                <CardDeck>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Web Development
-                        </Card.Title>
-                            <Card.Text>
-                                <Row>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faHtml5} className="skills_icon" />
-                                        <span className="skills_lang">HTML</span>
-                                    </Col>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faCss3Alt} className="skills_icon" />
-                                        <span className="skills_lang">CSS</span>
-                                    </Col>
-                                    <Col md={4} xs={12}>
-                                        <FontAwesomeIcon icon={faSass} className="skills_icon" />
-                                        <span className="skills_lang">Sass</span>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faBootstrap} className="skills_icon" />
-                                        <span className="skills_lang">Bootstrap</span>
-                                    </Col>
-                                    <Col md={4}xs={6}>
-                                        <FontAwesomeIcon icon={faJs} className="skills_icon" />
-                                        <span className="skills_lang">JavaScript</span>
-                                    </Col>
-                                    <Col md={4} xs={12}>
-                                        <FontAwesomeIcon icon={faReact} className="skills_icon" />
-                                        <span className="skills_lang">React + Redux</span>
-                                    </Col>
-                                </Row>
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="transparent-box one"></div>
-                    </Card>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Front-End Tools</Card.Title>
-                            <Card.Text>
-                                <Row>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faGitAlt} className="skills_icon" />
-                                        <span className="skills_lang">Git</span>
-                                    </Col>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faNpm} className="skills_icon" />
-                                        <span className="skills_lang">npm</span>
-                                    </Col>
-                                    <Col md={4} xs={12}>
-                                        <FontAwesomeIcon icon={faYarn} className="skills_icon" />
-                                        <span className="skills_lang">Yarn</span>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faJira} className="skills_icon" />
-                                        <span className="skills_lang">Jira</span>
-                                    </Col>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faSlack} className="skills_icon" />
-                                        <span className="skills_lang">Slack</span>
-                                    </Col>
-                                    <Col md={4} xs={12}>
-                                        <FontAwesomeIcon icon={faUsers} className="skills_icon" />
-                                        <span className="skills_lang">Scrum</span>
-                                    </Col>
-                                </Row>
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="transparent-box two"></div>
-                    </Card>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Learning Now</Card.Title>
-                            <Card.Text>
-                                <Row>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faNode} className="skills_icon" />
-                                        <span className="skills_lang">Node.js - Express</span>
-                                    </Col>
-                                    <Col md={4} xs={6}>
-                                        <FontAwesomeIcon icon={faEnvira} className="skills_icon" />
-                                        <span className="skills_lang">MongoDB</span>
-                                    </Col>
-                                    <Col md={4} xs={12}>
-                                        <FontAwesomeIcon icon={faLayerGroup} className="skills_icon" />
-                                        <span className="skills_lang">Stack MERN</span>
-                                    </Col>
-                                </Row>
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="transparent-box three"></div>
-                    </Card>
-                </CardDeck>
-            </div>
-        );
-    };
-};
+  render() {
+    return (
+      <div className="body skills">
+        <CardDeck>
+          <Card>
+            <Card.Body>
+              <Card.Title>Web Development</Card.Title>
+              <Card.Text>
+                <Row>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faHtml5} className="skills_icon" />
+                    <span className="skills_lang">HTML</span>
+                  </Col>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faCss3Alt} className="skills_icon" />
+                    <span className="skills_lang">CSS</span>
+                  </Col>
+                  <Col md={4} xs={12}>
+                    <FontAwesomeIcon icon={faSass} className="skills_icon" />
+                    <span className="skills_lang">Sass</span>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon
+                      icon={faBootstrap}
+                      className="skills_icon"
+                    />
+                    <span className="skills_lang">Bootstrap</span>
+                  </Col>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faJs} className="skills_icon" />
+                    <span className="skills_lang">JavaScript</span>
+                  </Col>
+                  <Col md={4} xs={12}>
+                    <FontAwesomeIcon icon={faReact} className="skills_icon" />
+                    <span className="skills_lang">React + Redux</span>
+                  </Col>
+                </Row>
+              </Card.Text>
+            </Card.Body>
+            <div className="transparent-box one"></div>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Front-End Tools</Card.Title>
+              <Card.Text>
+                <Row>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faGitAlt} className="skills_icon" />
+                    <span className="skills_lang">Git</span>
+                  </Col>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faNpm} className="skills_icon" />
+                    <span className="skills_lang">npm</span>
+                  </Col>
+                  <Col md={4} xs={12}>
+                    <FontAwesomeIcon icon={faYarn} className="skills_icon" />
+                    <span className="skills_lang">Yarn</span>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faJira} className="skills_icon" />
+                    <span className="skills_lang">Jira</span>
+                  </Col>
+                  <Col md={4} xs={6}>
+                    <FontAwesomeIcon icon={faSlack} className="skills_icon" />
+                    <span className="skills_lang">Slack</span>
+                  </Col>
+                  <Col md={4} xs={12}>
+                    <FontAwesomeIcon icon={faUsers} className="skills_icon" />
+                    <span className="skills_lang">Scrum</span>
+                  </Col>
+                </Row>
+              </Card.Text>
+            </Card.Body>
+            <div className="transparent-box two"></div>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Learning Now</Card.Title>
+              <Card.Text>
+                <Row>
+                  <Col md={4} xs={6}>
+                    <img className="skills_icon" src={Chakra} alt="Logo of Chakra-ui" />
+                    {/* <FontAwesomeIcon icon={faNode} className="skills_icon" /> */}
+                    {/* <span className="skills_lang">Node.js - Express</span> */}
+                  </Col>
+                  <Col md={4} xs={6}>
+                  <img className="skills_icon" src={Jwt} alt="Jason Web Token" />
+                    {/* <FontAwesomeIcon icon={faEnvira} className="skills_icon" /> */}
+                    <span className="skills_lang">Jason Web Token</span> 
+                  </Col>
+                  <Col md={4} xs={12}>
+                  <img className="skills_icon" src={Msw} alt="Mock Service Worker" />
+                    <span className="skills_lang">Mock Svice Worker</span>
+                  </Col>
+                  </Row>
+                  <Row>
+                  <Col md={4} xs={6}>
+                  <img className="skills_icon" src={Cypress} alt="Cypress" />
+                    {/* <FontAwesomeIcon icon={faEnvira} className="skills_icon" />
+                    <span className="skills_lang">MongoDB</span> */}
+                  </Col>
+                  <Col md={4} xs={6}>
+                  <img className="skills_icon" src={Storybook} alt="Cypress" />
+                    {/* <FontAwesomeIcon icon={faEnvira} className="skills_icon" />
+                    <span className="skills_lang">MongoDB</span> */}
+                  </Col>
+                  <Col md={4} xs={12}>
+                  <img className="skills_icon" src={Swagger} alt="Cypress" />
+                    {/* <FontAwesomeIcon icon={faEnvira} className="skills_icon" />
+                    <span className="skills_lang">MongoDB</span> */}
+                  </Col>
+                </Row>
+              </Card.Text>
+            </Card.Body>
+            <div className="transparent-box three"></div>
+          </Card>
+        </CardDeck>
+      </div>
+    );
+  }
+}
 
 export default About;
